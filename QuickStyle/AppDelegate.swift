eedd9dc15bb5527c4fc4d3f36333491f7e97af31
Derell Pigram <derell.pigram@ingeniousmed.com>
2016-10-25 22:59:11 -0400

    Added log in functionality

--- QuickStyle/AppDelegate.swift
+++ QuickStyle/AppDelegate.swift
@@ -16,8 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        let viewController = ViewController()
-        self.window?.rootViewController = viewController;
+        let loginScreen = LoginScreen()
+        self.window?.rootViewController = loginScreen;
         
         return true
     }
