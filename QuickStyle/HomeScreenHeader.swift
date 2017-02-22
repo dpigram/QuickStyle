f54b4db9e226194674fa2278685d93e4df739a16
Terell Pigram <terell.pigram@gmail.com>
2017-02-21 23:51:07 -0500

    Pushing latest changes

--- QuickStyle/HomeScreenHeader.swift
+++ QuickStyle/HomeScreenHeader.swift
@@ -8,10 +8,36 @@
 
 import UIKit
 
-class HomeScreenHeader: UIView {
+class HomeScreenHeader: UIView, UICollectionViewDataSource, UICollectionViewDelegate {
     public var data: AnyObject?
-    
+    @IBOutlet weak var dateCollectionView: UICollectionView!
+    @IBOutlet weak var leftButton: UIButton!
+    @IBOutlet weak var rightButton: UIButton!
+    override func awakeFromNib() {
+        print("working");
+    }
     public func loadData(data: AnyObject?) {
         // update ui
     }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = UICollectionViewCell()
+        
+        if(indexPath.row % 2 == 0){
+            cell.backgroundColor = UIColor.red
+        }
+        else{
+            cell.backgroundColor = UIColor.blue
+        }
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    } 
 }
---
+++ QuickStyle/HomeScreenHeaderCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  HomeScreenHeaderCollectionViewCell.swift
+//  QuickStyle
+//
+//  Created by Terell Pigram on 2/21/17.
+//  Copyright © 2017 Derell Pigram. All rights reserved.
+//
+
+import UIKit
+
+class HomeScreenHeaderCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
